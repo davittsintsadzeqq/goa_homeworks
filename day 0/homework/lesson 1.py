b68ae050481dd94f451e57708e42e7005c51f01d
davittsintsadzeqq <168895347+davittsintsadzeqq@users.noreply.github.com>
2024-05-10 21:58:18 +0400

    goa

---
+++ day 0/homework/lesson 1.py
@@ -0,0 +1,35 @@
+name = "davit"
+
+
+surname = "tsintsadze"
+
+print(name)
+
+
+
+
+name = "davit" #str string 
+age = 13 #es aris int (integer) mteli risxvi
+height = 150.5 #es aris float tipis svladi (atwiladi)
+#boolean (bool) tipis svladi
+
+knows_programing = True #true an falls
+is_ugli = False #snake case
+
+isUgli = False #java skript camelcase
+
+
+print(name + " " + surname)
+
+#stringi aris brjyalebsi mosemuli simboloebi
+#print(name + age)
+
+#print(type(age))
+
+
+
+
+
+print(name + " " +  str(age))
+
+print(name + " " +surname + " " + str(age) + " " + str(height))
