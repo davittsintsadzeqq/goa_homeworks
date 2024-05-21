1e54cc34ee1b96658ef7bbad59917e0b7615b388
davittsintsadzeqq <168895347+davittsintsadzeqq@users.noreply.github.com>
2024-05-21 21:36:39 +0400

    goa

---
+++ lvl_4/homework/homework 1.py
@@ -0,0 +1,40 @@
+name = "davit"
+
+surename = "tsintsadze"
+
+age = 13
+
+hobby = "programing"
+
+country = "georgia"
+
+print(name)
+
+print(surename)
+
+print(age)
+
+print(hobby)
+
+print(country)
+
+print(name + " " + surename + " " + str(age) + " " + hobby + " " + country)
+
+num1 = 4
+
+num2 = 8
+
+print(num1 + num2)
+
+print(num1 - num2)
+
+print(num1 *  num2)
+
+print(num1 / num2)
+
+print(num1 % num2) # nasti
+
+print(num1 // num2) # ramdenjer tavsdeba
+
+print(num1 ** num2) #xarisxsi ayvana
+
