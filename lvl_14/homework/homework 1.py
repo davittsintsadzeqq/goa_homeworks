3c66832026eec454cef497bd931ed395fe12ec46
davittsintsadzeqq <davittsintsadze2@gmail.com>
2024-06-26 18:13:51 +0400

    GOA

---
+++ lvl_14/homework/homework 1.py
@@ -0,0 +1,19 @@
+print(True and True)
+
+print(True and False)
+
+print(False and False)
+
+print(False and True)
+
+print(False or False)
+
+print(True or True)
+
+print(False or True)
+
+print(True or False)
+
+
+
+
