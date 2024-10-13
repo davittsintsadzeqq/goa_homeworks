4e18b5d4ba66332aa543e664c3a0719ef044f460
davittsintsadzeqq <davittsintsadze2@gmail.com>
2024-10-13 19:30:32 +0400

    goa

---
+++ lvl_41/classwork/Func.py
@@ -0,0 +1,37 @@
+
+#def say_hai():
+    #print("Hi")
+
+#say_hai()
+
+
+# def print_bill(custumer_name):
+  # print("======")
+  # print(custumer_name)
+  # print("======")
+
+# user_input = input("Enter custumer name:")
+
+# print_bill(user_input)
+
+# def welcome(name):
+  # print("welcome", name)
+
+# welcome(input("Enter your name:"))
+
+
+
+
+# def say_hi(name):
+     # return f"Hello, {name}"
+
+# res = say_hi("David")
+
+x=int(input("Enter number:"))
+y=int(input("Enter number:"))
+
+def area(x, y):
+
+  print(x + y)
+
+area(x,y)
